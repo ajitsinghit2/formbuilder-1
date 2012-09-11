2e846e2428f133b8d9cbdf0136a5ec626f2976f2
John Reeves <vectorjohn@gmail.com>
2012-09-10 22:26:46 -0700

    saving work

---
+++ formbuilder.js
@@ -0,0 +1,13 @@
+(function( $, undefined ) {
+
+    $.widget("ui.formbuilder", {
+
+        options: {
+        },
+
+        _create: function() {
+        }
+
+    });
+
+}( jQuery ));
---
+++ test/basic.js
@@ -0,0 +1,3 @@
+$(function(){
+    $('#builder').formbuilder();
+});
